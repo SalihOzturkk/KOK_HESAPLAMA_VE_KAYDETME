cc33db0765a7c07b3e45842d7541f83a3acd33a9
Salih Öztürk <153739749+SalihOzturkk@users.noreply.github.com>
2024-11-30 16:19:51 +0300

    REEEL KOK HESAPLAMA VE SİSTEME KAYDETME
    REEEL KOK HESAPLAMA VE SİSTEME KAYDETME

---
+++ 2024.10.14/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace _2024._10._14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(72, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(91, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(72, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(169, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(72, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(247, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(200, 160);
+            this.listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(453, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(230, 160);
+            this.listBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(229, 70);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "HESAPLA ve KAYDET";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 140);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(189, 134);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(453, 178);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(230, 70);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "KAYDEDİLENİ OKU";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 22);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(171, 60);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "SAYILARI İSTE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(6, 88);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(171, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(695, 315);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 2024.10.14/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic; //inputbox ı kullanabilmek için bunu tanımlamamız lazım
+//inputbox ı açmak için referanslardan visualbasic i eklemek gerekiyor
+
+namespace _2024._10._14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = char.IsLetter(e.KeyChar);
+
+
+
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = char.IsLetter(e.KeyChar);
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = char.IsLetter(e.KeyChar);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //if (textBox1.Text != "")
+            //{ 
+            //if (Convert.ToInt32(textBox1.Text) < 1 ||
+            //    Convert.ToInt32(textBox1.Text) > 9)
+            //{
+            //    MessageBox.Show("1 - 9 ARASINDA SAYI SEÇİNİZ"); 
+            //}
+            
+            //}
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            //if (textBox2.Text != "")
+            //{
+            //    if (Convert.ToInt32(textBox2.Text) < 1 ||
+            //        Convert.ToInt32(textBox2.Text) > 9)
+            //    {
+            //        MessageBox.Show("1 - 9 ARASINDA SAYI SEÇİNİZ");
+            //    }
+
+            //}
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            //if (textBox3.Text != "")
+            //{
+            //    if (Convert.ToInt32(textBox3.Text) < 1 ||
+            //        Convert.ToInt32(textBox3.Text) > 9)
+            //    {
+            //        MessageBox.Show("1 - 9 ARASINDA SAYI SEÇİNİZ");
+            //    }
+
+            //}
+        }
+        void temizle()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+
+        double a, b, c, delta, tekkok, x1, x2;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                a = Convert.ToDouble(textBox1.Text);
+                b = Convert.ToDouble(textBox2.Text);
+                c = Convert.ToDouble(textBox3.Text);
+
+                delta = (b * b) - (4 * a * c);
+
+                if (delta < 0)
+                {
+                    MessageBox.Show("FONKSİYONUN REEL KÖKLERİ YOKTUR!!!");
+                }
+                if (delta == 0)
+                {
+                    tekkok = (-1 * b) / (2 * a);
+                    StreamWriter kaydet1;
+                    kaydet1 = File.AppendText("tekkok.txt");
+                    kaydet1.WriteLine("A = " + a + " " + "B = " + b + " " +
+                       "C = " + c + " " + "Tekkök = " + tekkok);
+                    kaydet1.WriteLine("----------------------------------------------");
+                    kaydet1.Close();
+                    listBox1.Items.Add("A = " + a + " " + "B = " + b + " " +
+                       "C = " + c + " " + "Tekkök = " + tekkok);
+                    MessageBox.Show("Kök tekkok.txt Dosyasına kaydedildi!!");
+                }
+                if (delta > 0)
+                {
+                    x1 = ((-1 * b) + Math.Sqrt(delta)) / (2 * a);
+                    x2 = ((-1 * b) - Math.Sqrt(delta)) / (2 * a);
+                    StreamWriter kaydet2;
+                    kaydet2 = File.AppendText("ciftkok.txt");
+                    kaydet2.WriteLine("A = " + a + " " + "B = " + b + " " +
+                       "C = " + c + " " + "X1 = "+Math.Round (x1)+" "+"X2 = "+Math.Round( x2));
+                    kaydet2.WriteLine("----------------------------------------------");
+                    kaydet2.Close();
+                    listBox1.Items.Add("A = " + a + " " + "B = " + b + " " +
+                       "C = " + c + " " + "X1 = " + Math.Round(x1) + " " + "X2 = " + Math.Round(x2));
+                    MessageBox.Show("Kök ciftkok.txt Dosyasına kaydedildi!!");
+                }
+                
+            }
+            catch
+            {
+                MessageBox.Show("SAYISAL DEĞER GİRİNİZ!!!");
+            }
+            finally
+            {
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+            }
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            string veriler;    StreamReader oku;
+            oku = File.OpenText("ciftkok.txt");
+            while((veriler=oku.ReadLine()) !=null)
+            {
+                listBox2.Items.Add(veriler);
+            }
+            oku.Close();
+        }
+        Int32 deger;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //inputbox ı çağımak için interaction yazmak gerekiyor
+                deger = Convert.ToInt32(Interaction.InputBox("Herhangi bir sayı giriniz",
+                    "SAYI GİRİŞİ", "Buraya yazınız"));
+                textBox4.Text = "Girilen Sayı  :" + deger;
+
+            }
+            catch
+            {
+                MessageBox.Show("LÜTFEN SAYI GİRİNİZ");
+            }
+        }
+    }
+}
